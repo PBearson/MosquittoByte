b08e4da9b95aff86649126e2d02709045d4ef72c
PBearson <peabryan95@gmail.com>
2021-06-22 12:24:18 -0400

    Added packet class

---
+++ packet.py
@@ -0,0 +1,24 @@
+# TODO add toList() and toString() super functions
+# TODO add payload, payload_length objects
+
+class Packet:
+    def __init__(self):
+        self.payload = []
+        self.payload_length = 0
+
+    def toList(self):
+        l = []
+        for p in self.payload:
+            for n in p:
+                if type(n) == list:
+                    for x in n:
+                        l.append(x)
+                else:
+                    l.append(n)
+        return l
+        
+    def toString(self):
+        return "".join(self.toList())
+
+    def getByteLength(self):
+        return len(self.toString()) / 2
