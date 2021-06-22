f46e0628bf47691132b8b6e3e9054776ed122896
PBearson <peabryan95@gmail.com>
2021-06-22 12:54:41 -0400

    Fixed decoding from hex bugs

--- connect.py
+++ connect.py
@@ -10,24 +10,24 @@ class ConnectProperties(Packet):
     def __init__(self):
 
         self.property_length = 0
-        self.session_expiry_interval = ["%.2x" % 0x11, "%.4x" % random.getrandbits(32)]
-        self.receive_maximum = ["%.2x" % 0x21, "%.2x" % max(1, random.getrandbits(16))]
-        self.maximum_packet_size = ["%.2x" % 0x27, "%.4x" % max(1, random.getrandbits(32))]
-        self.topic_alias_maximum = ["%.2x" % 0x22, "%.2x" % random.getrandbits(16)]
+        self.session_expiry_interval = ["%.2x" % 0x11, "%.8x" % random.getrandbits(32)]
+        self.receive_maximum = ["%.2x" % 0x21, "%.4x" % max(1, random.getrandbits(16))]
+        self.maximum_packet_size = ["%.2x" % 0x27, "%.8x" % max(1, random.getrandbits(32))]
+        self.topic_alias_maximum = ["%.2x" % 0x22, "%.4x" % random.getrandbits(16)]
         self.request_response_information = ["%.2x" % 0x19, "%.2x" % random.getrandbits(1)]
         self.request_problem_information = ["%.2x" % 0x17, "%.2x" % random.getrandbits(1)]
-        self.user_property_name_len = random.randint(1, 20)
+        self.user_property_name_len = 1#random.randint(1, 20)
         self.user_property_name = ["%.2x" % ord(random.choice(string.printable)) for i in range(self.user_property_name_len)]
-        self.user_property_value_len = random.randint(1, 20)
+        self.user_property_value_len = 1#random.randint(1, 20)
         self.user_property_value = ["%.2x" % ord(random.choice(string.printable)) for i in range(self.user_property_value_len)]
         self.user_property = ["%.2x" % 0x26, "%.2x" % self.user_property_name_len, self.user_property_name, "%.2x" % self.user_property_value_len, self.user_property_value]
-        self.authentication_method_len = random.randint(1, 20)
+        self.authentication_method_len = 1#random.randint(1, 20)
         self.authentication_method = ["%.2x" % 0x15, "%.2x" % self.authentication_method_len, ["%.2x" % ord(random.choice(string.printable)) for i in range(self.authentication_method_len)]]
-        self.authentication_data_len = random.randint(1, 100)
+        self.authentication_data_len = 1#random.randint(1, 100)
         self.authentication_data = ["%.2x" % 0x16, "%.2x" % self.authentication_data_len, "%.2x" % random.getrandbits(8 * self.authentication_data_len)]
 
         self.payload = [self.property_length]
-        properties_bitmap = random.getrandbits(9)
+        properties_bitmap = 0#random.getrandbits(9)
 
         if properties_bitmap & 1 == 0:
             self.payload.append(self.session_expiry_interval)
@@ -91,7 +91,7 @@ class ConnectVariableHeader(Packet):
         self.name = ["%.2x" % 0b0, "%.2x" % 0b100, "%.2x" % 0b1001101, "%.2x" % 0b1010001, "%.2x" % 0b1010100, "%.2x" % 0b1010100]
         self.protocol_version = ["%.2x" % random.choice([0b11, 0b100, 0b101])]
         self.flags = ConnectFlags()
-        self.keepalive = ["%.2x" % random.getrandbits(16)]
+        self.keepalive = ["%.4x" % random.getrandbits(16)]
         self.properties = ConnectProperties()
 
         self.payload = [self.name, self.protocol_version, self.flags.toList(), self.keepalive, self.properties.toList()]
@@ -108,7 +108,7 @@ class WillProperties(Packet):
         self.response_topic_length = random.randint(1, 20)
         self.response_topic = ["%.2x" % 0x08, "%.4x" % self.response_topic_length, ["%.2x" % ord(random.choice(string.printable)) for i in range(self.response_topic_length)]]
         self.correlation_data_length = random.randint(1, 30)
-        self.correlation_data = ["%.2x" % 0x09, "%.4x" % self.correlation_data_length, "%x" % random.getrandbits(8 * self.correlation_data_length)]
+        self.correlation_data = ["%.2x" % 0x09, "%.4x" % self.correlation_data_length, ["%.2x" % random.getrandbits(8) for i in range(self.correlation_data_length)]]
         self.user_property_name_len = random.randint(1, 20)
         self.user_property_name = ["%.2x" % ord(random.choice(string.printable)) for i in range(self.user_property_name_len)]
         self.user_property_value_len = random.randint(1, 20)
@@ -157,7 +157,7 @@ class ConnectPayload(Packet):
         self.will_topic_length = random.randint(1, 30)
         self.will_topic = ["%.4x" % self.will_topic_length, ["%.2x" % ord(random.choice(string.printable)) for i in range(self.will_topic_length)]]
         self.will_payload_length = random.randint(1, 100)
-        self.will_payload = ["%.4x" % self.will_payload_length, "%x" % random.getrandbits(8 * self.will_payload_length)]
+        self.will_payload = ["%.4x" % self.will_payload_length, ["%.2x" % random.getrandbits(8) for i in range(self.will_payload_length)]]
         self.username_length = random.randint(1, 20)
         self.username = ["%.4x" % self.username_length, ["%.2x" % ord(random.choice(string.printable)) for i in range(self.username_length)]]
         self.password_length = random.randint(1, 20)
@@ -188,7 +188,7 @@ class Connect(Packet):
         
         self.payload_length = 4 + self.variable_header.payload_length + self.connect_payload.payload_length
 
-        self.payload = [self.fixed_header, "%.2x" % self.payload_length, self.variable_header.toList(), self.connect_payload.toList()]
+        self.payload = [self.fixed_header, "%.4x" % self.payload_length, self.variable_header.toList(), self.connect_payload.toList()]
 
     def printComponentsAsList(self):
         print("Fixed header: ", self.fixed_header)
@@ -201,12 +201,12 @@ class Connect(Packet):
 
     def printComponentSizes(self):
         print("Fixed header: 2 bytes")
-        print("Connect flags: %d bytes" % self.variable_header.flags.getByteLength())
-        print("Connect properties: %d bytes" % self.variable_header.properties.getByteLength())
-        print("Variable header: %d bytes" % self.variable_header.getByteLength())
-        print("Will properties: %d bytes" % self.connect_payload.will_properties.getByteLength())
-        print("Payload: %d bytes" % self.connect_payload.getByteLength())
-        print("Final packet: %d bytes" % self.getByteLength())
+        print("Connect flags: %.1f bytes" % self.variable_header.flags.getByteLength())
+        print("Connect properties: %.1f bytes" % self.variable_header.properties.getByteLength())
+        print("Variable header: %.1f bytes" % self.variable_header.getByteLength())
+        print("Will properties: %.1f bytes" % self.connect_payload.will_properties.getByteLength())
+        print("Payload: %.1f bytes" % self.connect_payload.getByteLength())
+        print("Final packet: %.1f bytes" % self.getByteLength())
 
 def test():
     host = "127.0.0.1"
@@ -218,9 +218,14 @@ def test():
         packet.printComponentSizes()     
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((host, port))
-        s.send(bytearray.fromhex(packet.toString()))
+        try:
+            s.send(bytearray.fromhex(packet.toString()))
+        except ValueError:
+            print("Error caused by following payload:")
+            print(packet.toString())
+            exit(0)
         s.close()
-        time.sleep(1)
+        time.sleep(0.25)
 
 if __name__ == "__main__":
     test()
