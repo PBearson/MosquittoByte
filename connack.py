6870b4deac60e90cde2c40c14ac55c5012f3cb3c
PBearson <peabryan95@gmail.com>
2021-06-24 13:49:09 -0400

    Cleanup

--- connack.py
+++ connack.py
@@ -7,17 +7,15 @@ class ConnackProperties(Packet):
     def __init__(self):
         super().__init__()
 
-        self.session_expiry_interval = ["%.2x" % 0x11, "%.8x" % random.getrandbits(32)]
-        self.receive_maximum = ["%.2x" % 0x21, "%.4x" % max(1, random.getrandbits(16))]
-        self.maximum_qos = ["%.2x" % 0x24, "%.2x" % random.getrandbits(1)]
-        self.retain_available = ["%.2x" % 0x25, "%.2x" % random.getrandbits(1)]
-        self.maximum_packet_size = ["%.2x" % 0x27, "%.8x" % max(1, random.getrandbits(32))]
+        self.session_expiry_interval = self.toBinaryData(0x11, 4, True)
+        self.receive_maximum = self.toBinaryData(0x21, 2, True, 8, 1)
+        self.maximum_qos = self.toBinaryData(0x24, 1, True, 1)
+        self.retain_available = self.toBinaryData(0x25, 1, True, 1)
+        self.maximum_packet_size = self.toBinaryData(0x27, 4, True, 8, 1)
         self.assigned_client_id_length = random.randint(1, 30)
-        self.assigned_client_id = ["%.2x" % 0x12, "%.4x" % self.assigned_client_id_length, self.getAlphanumHexString(self.assigned_client_id_length)]
-        self.topic_alias_maximum = ["%.2x" % 0x22, "%.4x" % random.getrandbits(16)]
-        # self.
+        self.assigned_client_id = self.toEncodedString(0x12, self.assigned_client_id_length)
+        self.topic_alias_maximum = self.toBinaryData(0x22, 2, True)
 
-        # print(self.assigned_client_id)
         self.payload_length = 5 + 3 + 4 + 4 + 5 + 3 + self.assigned_client_id_length
         self.payload = [self.toVariableByte("%x" % self.payload_length), self.session_expiry_interval, self.receive_maximum, self.maximum_qos, self.retain_available, self.maximum_packet_size, self.assigned_client_id]
 
@@ -26,11 +24,10 @@ class ConnackVariableHeader(Packet):
     def __init__(self, connect_packet):
         super().__init__()
 
-        self.acknowledgement_flags = ["%.2x" % random.getrandbits(1)]
-        self.return_code = ["%.2x" % random.getrandbits(8)]
+        self.acknowledgement_flags = self.toBinaryData(None, 1, True, 1)
+        self.return_code = self.toBinaryData(None, 1, True)
         self.connack_properties = ConnackProperties()
 
-        # print(connect_packet.variable_header.protocol_version)
 
         self.payload = [self.acknowledgement_flags, self.return_code, self.connack_properties.toList()]
         self.payload_length = 2 + self.connack_properties.payload_length - 3
@@ -53,7 +50,7 @@ def test():
     host = "127.0.0.1"
     port = 1883
 
-    for i in range(10):
+    for i in range(100):
         packet = Connack()
         packet.sendToBroker(host, port)
         print(packet.toString())
