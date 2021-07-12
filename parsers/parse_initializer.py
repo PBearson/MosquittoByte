16b30a9b56c9e1dd51b3dfa814958dc84e56ecf1
PBearson <peabryan95@gmail.com>
2021-07-12 18:26:07 -0400

    Added puback parser

--- parsers/parse_initializer.py
+++ parsers/parse_initializer.py
@@ -6,9 +6,11 @@ sys.path.append("generators")
 from connack_parser import ConnackParser
 from publish_parser import PublishParser
 from disconnect_parser import DisconnectParser
+from puback_parser import PubackParser
 from connack import Connack
 from publish import Publish
 from connect import Connect
+from puback import Puback
 from disconnect import Disconnect
 from packet import sendToBroker
 import random
@@ -20,13 +22,15 @@ class ParseInitializer:
             self.parser = ConnackParser(payload, protocol_version)
         elif payload[0] == '3':
             self.parser = PublishParser(payload, protocol_version)
+        elif payload[0] == '4':
+            self.parser = PubackParser(payload, protocol_version)
         elif payload[0] == 'e':
             self.parser = DisconnectParser(payload, protocol_version)
 
 def test():
     protocol_version = random.randint(3, 5)
     connect = Connect(protocol_version)
-    payload = Disconnect(protocol_version)
+    payload = Puback(protocol_version)
     sendToBroker("localhost", 1883, connect.toString() + payload.toString())
     parser = ParseInitializer(payload.toString(), protocol_version).parser
     g_fields = parser.G_fields
