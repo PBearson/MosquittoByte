9ef3b5588cf00069cd3fda91b07d0ad8c0ef919e
PBearson <peabryan95@gmail.com>
2021-07-12 14:00:52 -0400

    Updated connack parser

--- parsers/connack_parser.py
+++ parsers/connack_parser.py
@@ -4,11 +4,9 @@ class ConnackParser(Parser):
     def __init__(self, payload, protocol_version):
         super().__init__(payload, protocol_version)
 
-        self.G_fields["acknowledge flags"] = self.indexToByte()
-        self.index += 2
+        self.index = self.insertByteNoIdentifier("acknowledge flags", payload, self.index, True)
 
-        self.G_fields["reason code"] = self.indexToByte()
-        self.index += 2
+        self.index = self.insertByteNoIdentifier("reason code", payload, self.index, True)
 
         if protocol_version == 5:
             self.parseProperties()
