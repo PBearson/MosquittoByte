e2ec3c98225f7074573decdb22285098128ae651
PBearson <peabryan95@gmail.com>
2021-07-11 21:10:53 -0400

    Parse properties for MQTTv5

--- parsers/connack_parser.py
+++ parsers/connack_parser.py
@@ -3,10 +3,12 @@ from parser import Parser
 class ConnackParser(Parser):
     def __init__(self, payload, protocol_version):
         super().__init__(payload, protocol_version)
-        print(payload, protocol_version)
 
         self.G_fields["acknowledge flags"] = self.indexToByte()
         self.index += 2
 
         self.G_fields["reason code"] = self.indexToByte()
+        self.index += 2
 
+        if protocol_version == 5:
+            self.parseProperties()
